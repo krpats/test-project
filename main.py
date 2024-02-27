589608da16febc3cd4dd27b5ede935604436bf6e
krlives <32258722-krlives@users.noreply.replit.com>
2024-02-27 13:59:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# for i in range(10):
+#   print('Это предупреждение №', i)
+
+# for i in range(10):
+#   print('ало')
+#   for j in range(5):
+#     print('...ало..')
+
+# result = 0
+# for i in range(0, 101):
+#   print(i)
+#   result += i
+# print(result)
+
+# result = 0
+# for i in range(0, 101, 2):
+#   result += i
+# print(result
+
+
+result = 1
+for i in range(5, 16):
+  result *= i
+print(result)
+
+
+
+
+
+
